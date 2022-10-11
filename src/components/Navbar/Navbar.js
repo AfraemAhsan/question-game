48f3f6c676ad17f1332d5afe03e59a1de9d0bc11
afraemahsan <afraemahsan24@gmail.com>
2022-10-11 16:08:35 +0600

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <h3>Question-Game</h3>
+      </div>
+      <div></div>
+    </nav>
+  );
+};
+
+export default Navbar;
