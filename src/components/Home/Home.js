0c4519cc2d004bd86d0ea994b70bf1d336dacfcb
afraemahsan <afraemahsan24@gmail.com>
2022-10-12 00:35:35 +0600

    home updating second

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
+import Quiz from "./Quiz/Quiz";
 
 const Home = () => {
+  const quizes = useLoaderData();
+  console.log(quizes);
   return (
     <div>
-      <div className="flex flex-row p-[50px] justify-evenly">
+      <div className="grid  md:grid-cols-2 lg:grid-cols-3 grid-cols-1  p-[50px] justify-center mb-[20px]">
         <img
-          className="h-250px] w-[150px] object-cover mr-[30px] "
+          className="  md:h-250px] w-full md:w-[300px] object-cover mr-[30px] "
           src="https://img.freepik.com/premium-vector/quiz-time-red-yellow-text-effect-template-with-3d-type-style_7280-3951.jpg?"
           alt=""
         />
-        <div className="w-[1000px]">
+        <div className="w-[800px]">
           <p className="text-2xl font-semibold">
             Questions is a game that is played by participants maintaining a
             dialogue of asking questions back and forth for as long as possible,
@@ -17,6 +21,11 @@ const Home = () => {
             first player serves by asking a question.{" "}
           </p>
         </div>
+      </div>
+      <div className="grid  md:grid-cols-2 lg:grid-cols-3 grid-cols-1 ml-[10px] ">
+        {quizes.data.map((quize) => (
+          <Quiz key={quize.id} quize={quize}></Quiz>
+        ))}
       </div>
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
         <h3 className="text-3xl font-bold text-slate-200">Question-Game</h3>
       </div>
 
-      <div className="space-x-[20px] text-slate-200">
+      <div className="space-x-[20px] text-slate-200 text-base">
         <Link to="/">Home</Link>
         <Link to="/statistics">Statistics</Link>
         <Link to="/blog">Blog</Link>
